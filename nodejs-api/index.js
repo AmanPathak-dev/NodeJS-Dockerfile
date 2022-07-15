185fde6fc8988439ab9bac1bc57dc08cd5a4b4b9
AmanPathak-dev <aman07pathak@gmail.com>
2022-07-15 18:03:37 +0530

    Initial Commit

---
+++ nodejs-api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.json([
+	{
+		name: 'Bill',
+		email: 'billy.gates@gates.com'
+	},
+	{
+		name: 'Jeff',
+		email: 'bezos.jeff@bezos.com'
+	}
+]))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
